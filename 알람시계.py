054e97321ea0eb02d1d9743224fddc93e9193fe1
Woochan Jung <88023110+magnificentdud@users.noreply.github.com>
2021-07-29 13:41:53 +0900

    code correction

--- 알람시계.py
+++ 알람시계.py
@@ -1,9 +1,15 @@
-h = int(input("H: "))
-m = int(input("M: "))
-if m > 59 or h > 23:
-    break
+while true:
+    h = int(input("H: "))
+    m = int(input("M: "))
+    if m > 59 or h > 23:
 
 if m > 44:
     print (h, m-45)
-    else m<45:
-        print (h-1, 60-m)
+
+elif 
+
+
+
+
+else:
+    print (h-1, m +15)
