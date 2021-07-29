c05c324e3183aeea28bcb68c0e97f8d783c77aec
Woochan Jung <88023110+magnificentdud@users.noreply.github.com>
2021-07-29 13:03:27 +0900

    edit code

--- findingQuadrant.py
+++ findingQuadrant.py
@@ -1,14 +1,14 @@
-x = int(input())
--100 <= x <= 100, x != 0
-
-y = int(input())
--100 <= y <= 100, y != 0
-
+while True:
+    x = int(input("enter x: "))
+    y = int(input("enter y"))
+if x<100 and x>-100 and y< 100 and y > -100 and x!=0 and y ! = 0:
+    break
+print ("ERROR: NUMBERS OUT OF RANGE")
 if x > 0 and y > 0:
     print(1)
-if x > 0 and y<0:
+elif x > 0 and y<0:
     print(4)
-if x < 0 and y < 0:
+elif x < 0 and y < 0:
     print(3)
-if x < 0 and y > 0:
+else x < 0 and y > 0:
     print(2)
