885e7d18b3ef091e48293b9e80b1c4e522441eaf
Woochan Jung <88023110+magnificentdud@users.noreply.github.com>
2021-08-02 13:57:03 +0900

    redirect code

--- 알람시계.py
+++ 알람시계.py
@@ -1,9 +1,10 @@
 hour = int(input("Enter the hour: "))
-minute = int(input("Enger the minute: "))
+minute = int(input("Enter the minute: "))
 
 if minute > 44:
     print(hour, minute-45)
 elif minute <= 44 and hour >= 1:
     print(hour-1, minute+15)
 else:
-    print(23, minute+15)
+    print(23, minute+15)3
+    
