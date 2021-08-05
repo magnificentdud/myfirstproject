9aa14c35a0bae5db420bc6e9496df17a53bda9cc
Woochan Jung <88023110+magnificentdud@users.noreply.github.com>
2021-08-05 15:57:50 +0900

    a

---
+++ set practice.py
@@ -0,0 +1,11 @@
+my_set = {1,2,3,"hello", (100,200)}
+print(my_set)
+my_set[1] = 5
+
+my_tuple = (1,2,3)
+my_tuple[1] = 5
+
+my_set = {1,2,3,3,3,3}
+print(my_set)
+my_set = set([100,200])
+print(my_set)
