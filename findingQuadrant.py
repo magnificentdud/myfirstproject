31b7182f3d5abcb8b14f15f1554d8bf75dc2bdfc
Woochan Jung <88023110+magnificentdud@users.noreply.github.com>
2021-08-24 10:20:11 +0900

    Update findingQuadrant.py

--- findingQuadrant.py
+++ findingQuadrant.py
@@ -1,15 +1,15 @@
 while True:
     x = int(input("enter x: "))
     y = int(input("enter y"))
-if x < 100 and x > -100 and y < 100 and y > -100 and x!=0 and y! = 0:
-    break
+if x < 100 and x > -100 and y < 100 and y > -100 and x!=0 and y!= 0:
+    stop
 print ("ERROR: NUMBERS OUT OF RANGE")
 if x > 0 and y > 0:
     print(1)
-elif x > 0 and y<0:
+elif x > 0 and y < 0:
     print(4)
 elif x < 0 and y < 0:
     print(3)
-else x < 0 and y > 0:
+elif x < 0 and y > 0:
     print(2)
 
