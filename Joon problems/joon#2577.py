247f95f03087fd1edc5b9db7f6d66a19cd613531
Woochan Jung <88023110+magnificentdud@users.noreply.github.com>
2021-11-05 19:24:19 +0900

    aa

--- Joon problems/joon#2577.py
+++ Joon problems/joon#2577.py
@@ -1,7 +1,25 @@
-a = int(input())
-b = int(input())
-c = int(input())
-value = a*b*c
-value = str(value)
-for num in range(10):
-    print(value.count(str(num)))
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# value = a*b*c
+# value = str(value)
+# for num in range(10):
+#     print(value.count(str(num)))
+
+
+A = int(input())
+B = int(input())
+C = int(input())
+
+v = A*B*C
+count = [0] * 10
+
+v = str(v)
+
+
+for i in v:
+    i = int(i)
+    count[i] += 1
+for i in count:
+    print(i)
+    
